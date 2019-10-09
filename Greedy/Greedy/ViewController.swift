d64603ee90f7550a97ca5bd5accd6acdfa3f8c33
Luis Gustavo <luis.gustavo3013@hotmail.com>
2019-10-09 19:00:20 -0300

    Added empty xcodeproject

---
+++ Greedy/Greedy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Greedy
+//
+//  Created by Luis Gustavo Avelino de Lima Jacinto on 09/10/19.
+//  Copyright © 2019 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

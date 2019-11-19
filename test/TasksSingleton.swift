73863874e47985284cddde803120aa69095a7e21
Luis Gustavo <luis.gustavo3013@hotmail.com>
2019-11-18 21:02:42 -0300

    Deleted old code

--- test/TasksSingleton.swift
+++ test/TasksSingleton.swift
@@ -24,45 +24,44 @@ class TasksSingleton {
         // task 1
         dateComponents.hour = 1
         let sd1 = Calendar.current.date(from: dateComponents)!
-        dateComponents.hour = 2
+        dateComponents.hour = 4
         let ed1 = Calendar.current.date(from: dateComponents)!
-        tasks.append(Task(name: "Task 1", startDate: sd1, endDate: ed1))
+        tasks.append(Task(name: "Task 1", startDate: sd1, endDate: ed1, weight: 2))
 
         // task 2
-        dateComponents.hour = 1
+        dateComponents.hour = 2
         let sd2 = Calendar.current.date(from: dateComponents)!
-        dateComponents.hour = 3
+        dateComponents.hour = 6
         let ed2 = Calendar.current.date(from: dateComponents)!
-        tasks.append(Task(name: "Task 2", startDate: sd2, endDate: ed2))
+        tasks.append(Task(name: "Task 2", startDate: sd2, endDate: ed2, weight: 4))
 
         // task 3
-        dateComponents.hour = 2
+        dateComponents.hour = 5
         let sd3 = Calendar.current.date(from: dateComponents)!
-        dateComponents.hour = 4
+        dateComponents.hour = 7
         let ed3 = Calendar.current.date(from: dateComponents)!
-        tasks.append(Task(name: "Task 3", startDate: sd3, endDate: ed3))
+        tasks.append(Task(name: "Task 3", startDate: sd3, endDate: ed3, weight: 4))
 
         // task 4
         dateComponents.hour = 3
         let sd4 = Calendar.current.date(from: dateComponents)!
-        dateComponents.hour = 7
+        dateComponents.hour = 10
         let ed4 = Calendar.current.date(from: dateComponents)!
-        tasks.append(Task(name: "Task 4", startDate: sd4, endDate: ed4))
+        tasks.append(Task(name: "Task 4", startDate: sd4, endDate: ed4, weight: 7))
 
         // task 5
-        dateComponents.hour = 5
+        dateComponents.hour = 8
         let sd5 = Calendar.current.date(from: dateComponents)!
-        dateComponents.hour = 7
+        dateComponents.hour = 11
         let ed5 = Calendar.current.date(from: dateComponents)!
-        tasks.append(Task(name: "Task 5", startDate: sd5, endDate: ed5))
+        tasks.append(Task(name: "Task 5", startDate: sd5, endDate: ed5, weight: 2))
 
         // task 6
-        dateComponents.day = 2
-        dateComponents.hour = 6
+        dateComponents.hour = 9
         let sd6 = Calendar.current.date(from: dateComponents)!
-        dateComponents.hour = 7
+        dateComponents.hour = 12
         let ed6 = Calendar.current.date(from: dateComponents)!
-        tasks.append(Task(name: "Task 6", startDate: sd6, endDate: ed6))
+        tasks.append(Task(name: "Task 6", startDate: sd6, endDate: ed6, weight: 1))
 
     }
 }
